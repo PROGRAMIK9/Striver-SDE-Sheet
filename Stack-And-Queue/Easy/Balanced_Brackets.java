141c5821283738dd80215fcee7ca8fa4d839cb69
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-06-06 15:42:17 +0530

    Implement Balanced Brackets validation algorithm

---
+++ Stack-And-Queue/Easy/Balanced_Brackets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
+        Stack<Character> stk = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            }else{
+                if(stk.isEmpty()) return false;
+                if( (c == ')' && stk.peek() == '(')
+                 || (c == '}' && stk.peek() == '{') 
+                 || (c == ']' && stk.peek() == '['))stk.pop();
+                else return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+}
