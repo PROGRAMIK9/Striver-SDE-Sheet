6863d808b8f7a52b752918c065dc66ff0106854e
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-07-24 18:43:07 +0530

    Create Number-of-Islands.java

---
+++ Graphs/Medium/Number-of-Islands.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void calcNum(char[][] grid, int i, int j){
+        if( i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j]=='0')return;
+        grid[i][j]='0';
+        calcNum(grid,i+1,j);
+        calcNum(grid,i-1,j);
+        calcNum(grid,i,j+1);
+        calcNum(grid,i,j-1);
+    }
+    public int numIslands(char[][] grid) {
+        int c = 0;
+        for(int i =0;i<grid.length;i++){
+            for(int j= 0 ;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    c++;
+                    calcNum(grid,i,j);
+                }
+            }
+        }
+        return c;
+    }
+}
